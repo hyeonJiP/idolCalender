64afac65df7c4dfd61dd887565dc0185513ce613
kjy <rla1014ss@gmail.com>
2023-03-13 18:07:21 +0900

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { getCookie, removeCookie, setCookie } from "./cookie/cookie";
 import moment from "moment";
 import { fetchingData } from "./cookie/backData";
 
+//commit
 function App() {
   const dispatch = useDispatch();
   const reduxUserToken = useSelector((state) => state.auth.userToken);
